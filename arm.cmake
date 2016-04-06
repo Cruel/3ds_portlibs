b75f2248d4f8347d48bd5e29b6cd5f44830bc878
krruzic <krruzic@gmail.com>
2016-04-05 21:13:40 -0600

    update Makefile to build mbedtls
    patch removes support for ipv6 as ctrulib is missing it as of now, also adds a few needed lines to the arm.cmake file for lib/includes

--- arm.cmake
+++ arm.cmake
@@ -1,15 +1,37 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Generic)
 
+# some var defines
+SET(DEVKITARM /opt/devkitpro/devkitARM)
+SET(DEVKITPRO /opt/devkitpro)
+SET(CTRULIB ${DEVKITPRO}/libctru)
+
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER $ENV{DEVKITARM}/bin/arm-none-eabi-gcc)
 SET(CMAKE_CXX_COMPILER $ENV{DEVKITARM}/bin/arm-none-eabi-g++)
 SET(CMAKE_SIZE $ENV{DEVKITARM}/bin/arm-none-eabi-size)
 SET(CMAKE_OBJCOPY $ENV{DEVKITARM}/bin/arm-none-eabi-objcopy)
 
+# set the path to look for libs/includes
+SET(CMAKE_FIND_ROOT_PATH ${DEVKITARM} ${DEVKITPRO} ${CTRULIB} ${DEVKITPRO}/portlibs/armv6k )
+
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# might not be needed...
+include_directories(
+	${DEVKITPRO}/portlibs/3ds/include
+	${DEVKITPRO}/portlibs/armv6k/include
+	${CTRULIB}/include
+)
+
+# flags!
+SET(ARCH "-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft")
+SET(CPP3DS_ARM_FLAGS "-g -O2 ${ARCH} -ffunction-sections -fdata-sections")
+SET(CPP3DS_TEST_FLAGS "-g -O2")
+SET(CPP3DS_EMU_FLAGS "-g -O2")
